ab2f9cad3e118f4f626d875a9774c4059abda826
sogwujiakwu <sogwujiakwu@gmail.com>
2024-10-22 22:24:35 +0000

    updated

--- ansible_helper.tf
+++ ansible_helper.tf
@@ -3,14 +3,29 @@ data "template_file" "ansible_inventory" {
   template = file("${path.module}/templates/inventory.tftpl")
 
   vars = {
-    k8s_control_dns  = google_compute_instance.k8s_control.network_interface[0].access_config[0].nat_ip # public IP
-    k8s_control_ip   = google_compute_instance.k8s_control.network_interface[0].network_ip              # private IP
-    k8s_worker_dns     = google_compute_instance.k8s_worker.network_interface[0].access_config[0].nat_ip    # public IP
-    k8s_worker_ip      = google_compute_instance.k8s_worker.network_interface[0].network_ip                 # private IP
-    ansible_user    = "devopsokeke"
+    # Encode the complex data into JSON strings
+    k8s_controls = jsonencode([
+      for control in google_compute_instance.k8s_control :
+      {
+        dns = control.network_interface[0].access_config[0].nat_ip, # Public IP
+        ip  = control.network_interface[0].network_ip               # Private IP
+      }
+    ])
+
+    k8s_workers = jsonencode([
+      for worker in google_compute_instance.k8s_worker :
+      {
+        dns = worker.network_interface[0].access_config[0].nat_ip, # Public IP
+        ip  = worker.network_interface[0].network_ip               # Private IP
+      }
+    ])
+
+    ansible_user    = var.username
     ssh_private_key = local_file.openstack_ssh_key.filename
   }
 }
+
+
 
 # Output the generated inventory to a local file
 resource "local_file" "ansible_inventory" {
--- output.tf
+++ output.tf
@@ -1,19 +1,3 @@
-output "k8s_control_dns" {
-  value = google_compute_instance.k8s_control.network_interface[0].access_config[0].nat_ip
-}
-
-output "k8s_control_ip" {
-  value = google_compute_instance.k8s_control.network_interface[0].network_ip
-}
-
-output "k8s_worker_dns" {
-  value = google_compute_instance.k8s_worker.network_interface[0].access_config[0].nat_ip
-}
-
-output "k8s_worker_ip" {
-  value = google_compute_instance.k8s_worker.network_interface[0].network_ip
-}
-
 output "ssh_private_key" {
   value = local_file.openstack_ssh_key.filename
 }
@@ -22,4 +6,19 @@ output "ansible_inventory_content" {
   value = data.template_file.ansible_inventory.rendered
 }
 
+output "k8s_controls_dns" {
+  value = [for control in google_compute_instance.k8s_control : control.network_interface[0].access_config[0].nat_ip]
+}
+
+output "k8s_controls_ip" {
+  value = [for control in google_compute_instance.k8s_control : control.network_interface[0].network_ip]
+}
+
+output "k8s_workers_dns" {
+  value = [for worker in google_compute_instance.k8s_worker : worker.network_interface[0].access_config[0].nat_ip]
+}
+
+output "k8s_workers_ip" {
+  value = [for worker in google_compute_instance.k8s_worker : worker.network_interface[0].network_ip]
+}
 
